14d3bffe6709b398e15d3fe231c44226f7992509
Ethan Chen <intervigil@gmail.com>
2012-11-14 20:23:09 -0800

    holiday: include kernel as dependency

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ BOARD_PAGE_SIZE := 2048
 #TARGET_PREBUILT_KERNEL := device/htc/holiday/prebuilt/kernel
 #TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/holiday/recovery/recoverykernel
 TARGET_KERNEL_CONFIG := holiday_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/holiday
+TARGET_KERNEL_SOURCE := kernel/htc/msm8660
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
